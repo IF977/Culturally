bbe05cb12c1e72fa243ac9a13c73e70f7155d1ed
Gabriel D'Luca <gdsv@cin.ufpe.br>
2018-04-22 13:03:46 -0300

    Criando pages e configurando rotas

---
+++ Culturally/app/controllers/pages_controller.rb
@@ -0,0 +1,43 @@
+class PagesController < ApplicationController
+	def about
+	end
+
+	def team
+	end
+
+	def contact
+	end
+
+	def login
+	end
+
+	def signup
+	end
+
+	def password_reset
+	end
+
+	def search
+	end
+
+	def home
+	end
+
+	def profile
+	end
+
+	def profile_edit
+	end
+
+	def profile_following
+	end
+
+	def profile_followers
+	end
+
+	def profile_favorites
+	end
+
+	def admin
+	end
+end
---
+++ Culturally/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about' => 'pages#about'
+  get 'team' => 'pages#team'
+  get 'contact' => 'pages#contact'
+  get 'login' => 'pages#login'
+  get 'signup' => 'pages#signup'
+  get 'password_reset' => 'pages#password_reset'
+  get 'search' => 'pages#search'
+  get 'home' => 'pages#home'
+  get 'profile' => 'pages#profile'
+  get 'profile/edit' => 'pages#profile_edit'
+  get 'profile/following' => 'pages#profile_following'
+  get 'profile/followers' => 'pages#profile_followers'
+  get 'profile/favorites' => 'pages#profile_favorites'
+  get 'admin' => 'pages#admin'
+end
