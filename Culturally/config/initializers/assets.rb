33a567dd687102685ce1d5ad2e1fbdb150bc0c6a
Gabriel D'Luca <gdsv@cin.ufpe.br>
2018-05-13 22:33:32 -0300

    Adicionando tela de login

--- Culturally/config/initializers/assets.rb
+++ Culturally/config/initializers/assets.rb
@@ -20,6 +20,16 @@ Rails.application.config.assets.precompile += %w( slick/slick.css )
 Rails.application.config.assets.precompile += %w( slick/slick-theme.css )
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( main.css )
+Rails.application.config.assets.precompile += %w( util.css )
+Rails.application.config.assets.precompile += %w( vendor/daterangepicker/daterangepicker.css )
+Rails.application.config.assets.precompile += %w( vendor/select2/select2.min.css )
+Rails.application.config.assets.precompile += %w( vendor/animsition/css/animsition.min.css )
+Rails.application.config.assets.precompile += %w( vendor/css-hamburgers/hamburgers.min.css )
+Rails.application.config.assets.precompile += %w( vendor/bootstrap/css/bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( vendor/animate/animate.css )
+Rails.application.config.assets.precompile += %w( fonts/Linearicons-Free-v1.0.0/icon-font.min.css )
+Rails.application.config.assets.precompile += %w( fonts/font-awesome-4.7.0/css/font-awesome.min.css )
 
 # JAVASCRIPT
 Rails.application.config.assets.precompile += %w( jquery-1.11.3.min.js )
@@ -28,3 +38,16 @@ Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 Rails.application.config.assets.precompile += %w( datepicker.min.js )
 Rails.application.config.assets.precompile += %w( jquery.singlePageNav.min.js )
 Rails.application.config.assets.precompile += %w( slick/slick.min.js )
+Rails.application.config.assets.precompile += %w( vendor/jquery/jquery-3.2.1.min.js )
+Rails.application.config.assets.precompile += %w( vendor/animsition/js/animsition.min.js )
+Rails.application.config.assets.precompile += %w( vendor/bootstrap/js/popper.js )
+Rails.application.config.assets.precompile += %w( vendor/bootstrap/js/bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( vendor/select2/select2.min.js )
+Rails.application.config.assets.precompile += %w( vendor/daterangepicker/moment.min.js )
+Rails.application.config.assets.precompile += %w( vendor/daterangepicker/daterangepicker.js )
+Rails.application.config.assets.precompile += %w( vendor/countdowntime/countdowntime.js )
+Rails.application.config.assets.precompile += %w( main.js )
+
+
+
+
