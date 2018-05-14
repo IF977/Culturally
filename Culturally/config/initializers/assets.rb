df74435bbb285fd2de18d4b1ff4002b1f5cf1529
Gabriel D'Luca <gdsv@cin.ufpe.br>
2018-05-13 21:24:05 -0300

    Melhorando o visual da página Home. Closes #22

--- Culturally/config/initializers/assets.rb
+++ Culturally/config/initializers/assets.rb
@@ -12,3 +12,19 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# CSS
+Rails.application.config.assets.precompile += %w( tooplate-style.css )
+Rails.application.config.assets.precompile += %w( datepicker.css )
+Rails.application.config.assets.precompile += %w( slick/slick.css )
+Rails.application.config.assets.precompile += %w( slick/slick-theme.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+
+# JAVASCRIPT
+Rails.application.config.assets.precompile += %w( jquery-1.11.3.min.js )
+Rails.application.config.assets.precompile += %w( popper.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( datepicker.min.js )
+Rails.application.config.assets.precompile += %w( jquery.singlePageNav.min.js )
+Rails.application.config.assets.precompile += %w( slick/slick.min.js )
