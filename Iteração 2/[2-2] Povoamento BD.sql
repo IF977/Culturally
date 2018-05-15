e67d414f2bb2d710a79809381a8eea34a98e1c4f
Gabriel D'Luca <gdsv@cin.ufpe.br>
2018-05-14 23:32:27 -0300

    Adicionando scripts iniciais de povoamento previstos na issue #23

---
+++ Iteração 2/[2-2] Povoamento BD.sql
@@ -0,0 +1,31 @@
+# Entidades
+
+Insert into Visitante values ('117.131.130.161', 15.468750, -16.945478);
+Insert into Visitante values ('114.58.206.222', 42.832427, 101.953125);
+Insert into Visitante values ('83.178.96.196', -10.113873, -48.515625);
+Insert into Visitante values ('251.85.162.72', 19.005977, 81.562500);
+
+Insert into Conta values ('fulano@gmail.com', '567890a');
+Insert into Conta values ('stevejobs@apple.com', '098765b');
+Insert into Conta values ('mc@loma.com', '654321c');
+Insert into Conta values ('abcd@cin.ufpe.br', '123456d');
+
+Insert into Rede_Social values (1, 'Twitter');
+Insert into Rede_Social values (2, 'Facebook');
+Insert into Rede_Social values (3, 'Google+');
+
+
+# Especializações de conta
+
+
+# Especializações de roteiro cultural
+
+
+# Atributos multivalorados
+
+
+# Relacionamentos binários
+
+
+# Relacionamentos ternários
+
