c627953de6215f2e53c1f9077beb81c05f2b638c
Ana Lima <acl5@cin.ufpe.br>
2018-05-28 14:45:35 -0300

    Login com GoogleAuth

--- Culturally/config/routes.rb
+++ Culturally/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :places
-  devise_for :user, :path => '', :path_names => { :sign_in => "sign_in", :sign_out => "home", :sign_up => "sign_up" }
-
+  devise_for :user, :path => '', :path_names => { :sign_in => "sign_in", :sign_out => "home", :sign_up => "sign_up" }, controllers: { omniauth_callbacks: 'users/omniauth' }
+  
   root to: 'places#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about' => 'pages#about'
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get 'profile/followers' => 'pages#profile_followers'
   get 'profile/favorites' => 'pages#profile_favorites'
   get 'admin' => 'pages#admin'
-
+  
   #devise_scope :user do
    # get 'login', to: 'devise/sessions#new'
   #end
